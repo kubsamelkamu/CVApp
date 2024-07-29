a2b4e9b539896dff6cabd54c95e04321b737867f
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-07-29 23:12:36 +0300

    added br tag

--- src/Component/Education.jsx
+++ src/Component/Education.jsx
@@ -35,12 +35,14 @@ function EducationInfo(){
             <h1>Education Expreince</h1>
             {edit?(
                 <div className={styles.inputContainer}>
-                <label className={styles.label} htmlFor="schoolName">School-Name</label>
-                <input className={styles.input} value={schoolName}  type="text" id="schoolName" onChange={(e)=>e.target.value} />
-                <label className={styles.label} htmlFor="title">Title of study</label>
-                <input className={styles.input} value={title}  type="text" id="title" onChange={(e)=>e.target.value} />
-                <label className={styles.label} htmlFor="date">Date of Study</label>
-                <input className={styles.input} value={date}  type="date" id="date" onChange={(e)=>e.target.value} />
+                    <label className={styles.label} htmlFor="schoolName">School-Name</label>
+                    <input className={styles.input} value={schoolName}  type="text" id="schoolName" onChange={(e)=>e.target.value} />
+                    <br />
+                    <label className={styles.label} htmlFor="title">Title of study</label>
+                    <input className={styles.input} value={title}  type="text" id="title" onChange={(e)=>e.target.value} />
+                    <br />
+                    <label className={styles.label} htmlFor="date">Date of Study</label>
+                    <input className={styles.input} value={date}  type="date" id="date" onChange={(e)=>e.target.value} />
             </div>
             )}
             
