0cd9bb497d7c56ae39a2171ef7760bdfc96f5776
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-07-29 10:39:15 +0300

    created form input fields

--- src/Component/GeneralInfo.jsx
+++ src/Component/GeneralInfo.jsx
@@ -11,7 +11,26 @@ function GeneralInformation(){
 
     return(
         <div className="general-info">
-            
+            <label htmlFor="profile-picture">Profile</label>
+            <input type="filr" id="profile-picture" 
+            onChange={(e)=>setProfile(e.target.files[0])}
+            />
+            <img src={profile ? URL.createObjectURL(profile) : null} alt="Profile" />
+            <label htmlFor="firstName">First-Name:</label>
+            <input type="text" id="firstName" 
+            onChange={(fistName)=>setFirstName(fistName.target.value)}/>
+            <label htmlFor="middleName">Middle-Name</label>
+            <input type="text" id="middleName" 
+            onChange={(middleName)=>setMiddleName(middleName.target.value)}/>
+            <label htmlFor="lastName">Last Name:</label>
+            <input type="text" id="lastName" 
+            onChange={(lastName)=>setLastName(lastName.target.value)}/>
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" 
+            onChange={(email)=>setEmail(email.target.value)}/>
+            <label htmlFor="phoneNumber">Phone-Number</label>
+            <input type="tel" id="phoneNumber" 
+            onChange={(phoneNumber)=>setPhoneNumber(phoneNumber.target.value)}/>
         </div>
     )
 
