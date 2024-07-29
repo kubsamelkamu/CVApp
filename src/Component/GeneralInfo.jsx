3df73cda83d1edd721a39177cf708372e6c82103
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-07-29 11:25:16 +0300

    added classNames

--- src/Component/GeneralInfo.jsx
+++ src/Component/GeneralInfo.jsx
@@ -27,24 +27,29 @@ function GeneralInformation(){
     }
 
     return(
-        <div className="general-info">
+        <div className={styles.generalInfoContainer}>
             <label htmlFor="profile-picture">Profile</label>
             <input type="file" id="profile-picture " accept="image/*" 
             onChange={(e)=>setProfile(e.target.files[0])}
             />
-            {profile && <img src={profile ? URL.createObjectURL(profile) : null} alt="Profile" />}
-            <label htmlFor="firstName">First-Name:</label>
-            <input type="text" id="firstName" onChange={handleFirstNameChange}/>
-            <label htmlFor="middleName">Middle-Name</label>
-            <input type="text" id="middleName"  onChange={handleMiddleNameChange}/>
-            <label htmlFor="lastName">Last Name:</label>
-            <input type="text" id="lastName" onChange={handleLastNameChange}/>
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" onChange={handleEmailChange}/>
-            <label htmlFor="phoneNumber">Phone-Number</label>
-            <input type="tel" id="phoneNumber" onChange={handlePhoneNumberChange}/>
+            {profile && <img  className={styles.profilePicture} 
+            src={profile ? URL.createObjectURL(profile) : null} alt="Profile" />}
+
+            <div className={styles.inputContainer}>
+                <label className={styles.label} htmlFor="firstName">First-Name:</label>
+                <input className={styles.input} type="text" id="firstName" onChange={handleFirstNameChange}/>
+                <label className={styles.label} htmlFor="middleName">Middle-Name</label>
+                <input className={styles.input} type="text" id="middleName"  onChange={handleMiddleNameChange}/>
+                <label className={styles.label} htmlFor="lastName">Last Name:</label>
+                <input className={styles.input} type="text" id="lastName" onChange={handleLastNameChange}/>
+                <label className={styles.label} htmlFor="email">Email:</label>
+                <input className={styles.input} type="email" id="email" onChange={handleEmailChange}/>
+                <label className={styles.label} htmlFor="phoneNumber">Phone-Number</label>
+                <input className={styles.input} type="tel" id="phoneNumber" onChange={handlePhoneNumberChange}/>
+            </div>
+         
         </div>
-    )
+    );
 }
 
 export default GeneralInformation;
