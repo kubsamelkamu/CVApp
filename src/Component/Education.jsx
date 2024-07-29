f0bde8480db11134995f584e75bf1e6b2be54087
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-07-29 22:43:22 +0300

    added handle add Education

--- src/Component/Education.jsx
+++ src/Component/Education.jsx
@@ -6,5 +6,15 @@ function EducationInfo(){
     const[date,setDate] = useState('');
     const[edit,setEdit] = useState(true);
     const[educationList,setEducationList] = useState([]);
-    
+
+    const handleAddEducation=()=>{
+        if (schoolName && title && date){
+            const newEducationList = {schoolName,title,date};
+            setEducationList(...educationList,newEducationList);
+            setSchoolName('');
+            setDate('');
+            setTitle('');
+        }
+    }
+
 }
