70293e4d8522de7e980cfc3c643873ce6f72ed0b
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-07-29 22:39:01 +0300

    created states

---
+++ src/Component/Education.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+function EducationInfo(){
+    const[schoolName,setSchoolName] = useState('');
+    const[title,setTitle] = useState('');
+    const[date,setDate] = useState('');
+    const[edit,setEdit] = useState(true);
+    const[educationList,setEducationList] = useState([]);
+    
+}
