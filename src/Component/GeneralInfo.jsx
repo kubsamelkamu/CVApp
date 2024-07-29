23d1fa2cc121c0420a60a1c8debdeb57e5a6e64a
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-07-29 14:56:21 +0300

    updated with conditional rendering

--- src/Component/GeneralInfo.jsx
+++ src/Component/GeneralInfo.jsx
@@ -28,7 +28,7 @@ function GeneralInformation({onSubmit}){
     const handleEmailChange=()=>{
         setEmail(email);
     }
-    
+
     const handleSubmit =() =>{
         onSubmit({
             profile,
@@ -46,34 +46,57 @@ function GeneralInformation({onSubmit}){
 
     return(
         <div className={styles.generalInfoContainer}>
-            <label className={styles.label} htmlFor="profile-picture">Profile</label>
-            <input className={styles.input}  type="file" id="profile-picture " accept="image/*" 
-            onChange={(e)=>setProfile(e.target.files[0])}
-            />
-            {profile && <img  className={styles.profilePicture} 
-            src={profile ? URL.createObjectURL(profile) : null} alt="Profile" />}
-
-            <div className={styles.inputContainer}>
-                <label className={styles.label} htmlFor="firstName">First-Name:</label>
-                <input className={styles.input} type="text" id="firstName" onChange={handleFirstNameChange}/>
-                <br />
-                <label className={styles.label} htmlFor="middleName">Middle-Name</label>
-                <input className={styles.input} type="text" id="middleName"  onChange={handleMiddleNameChange}/>
-                <br />
-                <label className={styles.label} htmlFor="lastName">Last Name:</label>
-                <input className={styles.input} type="text" id="lastName" onChange={handleLastNameChange}/>
-                <br />
-                <label className={styles.label} htmlFor="email">Email:</label>
-                <input className={styles.input} type="email" id="email" onChange={handleEmailChange}/>
-                <br />
-                <label className={styles.label} htmlFor="phoneNumber">Phone-Number</label>
-                <input className={styles.input} type="tel" id="phoneNumber" onChange={handlePhoneNumberChange}/>
+            <h1>General Information</h1>
+            <div>
+                
             </div>
-                <br />
-                <div className={styles.buttons}>
-                    <button type="button">Submit</button>
-                    <button type="button">Edit</button>
+            {edit?(
+                
+                <>
+                <label className={styles.label} htmlFor="profile-picture">Profile</label><input className={styles.input} type="file" id="profile-picture " accept="image/*"
+                onChange={(e) => setProfile(e.target.files[0])} />
+                
+                {profile && <img  className={styles.profilePicture} 
+                src={profile ? URL.createObjectURL(profile) : null} alt="Profile" />}
+                
+                <div className={styles.inputContainer}>
+                    <label className={styles.label} htmlFor="firstName">First-Name:</label>
+                    <input className={styles.input} type="text" id="firstName" onChange={handleFirstNameChange}/>
+                    <br />
+                    <label className={styles.label} htmlFor="middleName">Middle-Name</label>
+                    <input className={styles.input} type="text" id="middleName"  onChange={handleMiddleNameChange}/>
+                    <br />
+                    <label className={styles.label} htmlFor="lastName">Last Name:</label>
+                    <input className={styles.input} type="text" id="lastName" onChange={handleLastNameChange}/>
+                    <br />
+                    <label className={styles.label} htmlFor="email">Email:</label>
+                    <input className={styles.input} type="email" id="email" onChange={handleEmailChange}/>
+                    <br />
+                    <label className={styles.label} htmlFor="phoneNumber">Phone-Number</label>
+                    <input className={styles.input} type="tel" id="phoneNumber" onChange={handlePhoneNumberChange}/>
                 </div>
+                  <br />
+                  <div className={styles.buttons}>
+                      <button type="button" onClick={handleSubmit}>Submit</button>
+                  </div>
+                </>
+            ):(
+                <div className={styles.submittedInfo}>
+                     {profile && <img src={profile} alt="Profile" className="profilePicture" />}
+                    <p>First-Name {fistName}</p>
+                    <p>Middle-Name {middleName}</p>
+                    <p>Last-Name {lastName}</p>
+                    <p>Email:{email}</p>
+                    <p>Phone-Number:{phoneNumber}</p>
+
+                    <div className={styles.buttons}>
+                        <button type="button" onClick={handleEdit}>Edit</button>
+                    </div>
+                </div>
+            )}
+           
+
+              
                       
         </div>
     );
