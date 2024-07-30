86ca04b63c8bbc4c41967397f1bd9552a7839ca3
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-07-30 09:39:47 +0300

    created state component

---
+++ src/Component/Practical.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function PracticalInfo(){
+    const[company,setCompany] = useState('');
+    const[position,setPosition]=useState('');
+    const[responsibilities,setResponsibilities] = useState('');
+    const[dateFrom,setDateFrom]=useState('');
+    const[dateTo,setDateTo]=useState('');
+    const[edit,setEdit]=useState('');
+    
+}
