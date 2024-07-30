a5b30b286df1834977d7c829370a34a43c63d41c
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-07-30 10:36:55 +0300

    imported files

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
-import GeneralInformation from './Component/GeneralInfo.jsx'
+
+import GeneralInformation from './Component/GeneralInfo'
+import EducationInfo from './Component/Education'
+import PracticalInfo from './Component/Practical'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GeneralInformation/>
+    <EducationInfo/>
+    <PracticalInfo/>
   </React.StrictMode>,
 )
